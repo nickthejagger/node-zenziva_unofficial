2661633f3386fc721a2134ac2d970232d830f257
nickthejagger <nichola.indra@gmail.com>
2018-10-04 08:36:57 +0700

    update

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ function zenziva_apismsmasking(user_key, passkey, phone_number,message){
     var options = {
         url: 'https://alpha.zenziva.net/apps/smsapi.php?userkey='+ user_key +'&passkey='+ passkey +'&nohp='+ phone_number +'&pesan=' + new_message
     }
-    return new Promise(function(resolve, reject) {
-        request.get(options, function(err, resp, body){
-            if (err){
-                reject(err);
-            } else {
-                resolve(body)
-            }
-        })
+    request.get(options, function(err, resp, body){
+        if(!err){
+            return(body)
+        } else{
+            throw err
+        }
     })
 }
 
@@ -24,16 +22,15 @@ function zenziva_apismsreguler(user_key, passkey, phone_number,message){
     var options = {
         url: 'https://reguler.zenziva.net/apps/smsapi.php?userkey='+ user_key +'&passkey='+ passkey +'&nohp='+ phone_number +'&pesan=' + new_message
     }
-    return new Promise(function(resolve, reject) {
-        request.get(options, function(err, resp, body){
-            if (err){
-                reject(err);
-            } else {
-                resolve(body)
-            }
-        })
+    request.get(options, function(err, resp, body){
+        if(!err){
+            return(body)
+        } else{
+            throw err
+        }
     })
 }
+
 
 module.exports.masking = zenziva_apismsmasking;
 module.exports.reguler = zenziva_apismsreguler;
